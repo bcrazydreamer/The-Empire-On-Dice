578e20f611e29b7e83f5cf631e7517d1af009faf
Ccrazydreamer <bcrazydreamer@gmail.com>
2018-11-11 05:52:52 +0530

    game ui and some validation added

--- public/js/game.js
+++ public/js/game.js
@@ -10,6 +10,11 @@ var valid_x = 0;
 var valid_y = 0;
 var c_turn = 1;
 var GameStarted = false;
+var introSoundEvent;
+var introSoundPlaying = true;
+var block_moving = false;
+var fx_On = true;
+var dice_rolled = false;
 function initGame(){
 	$.confirm({
     title: '"The Empire On Dice"',
@@ -20,7 +25,8 @@ function initGame(){
            btnClass: 'btn-success',
            action: function(){
            		GameStarted = true;
-              	setInterval(function(){
+           		$('#drop-block-btn').attr('disabled','true');
+              	introSoundEvent = setInterval(function(){
               		$('.castel-img').css('display','inline');
 					$("#introSound")[0].play();
 				}, 10);
@@ -29,19 +35,16 @@ function initGame(){
      }
   });
 }
-// if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-//  	var game_row = 25;
-// 	var game_col = 20;
-// 	createBoard();
-// }else{
-// 	var game_row = 35;
-// 	var game_col = 50;
-// 	createBoard();
-// }
-var game_row = 35;
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+	$('.mobile-btns').css('display','inline');
+ 	var game_row = 25;
+	var game_col = 20;
+	createBoard();
+}else{
+	var game_row = 35;
 	var game_col = 50;
 	createBoard();
-
+}
 function initGameVariables(){
 	l = 4;
 	b = 4;
@@ -63,19 +66,31 @@ function createBoard(){
 }
 
 function createEmpire(x,y,l,b,plr){
+	$('.placed-block').css('opacity','1');
 	color = (plr==1) ? 'red' : 'blue';
+	block_owner = (plr==1) ? 'p1_ownr' : 'p2_ownr';
 	for(var i=x+1 ; i <= x+l ; i++){
 		for(var k=x+1 ; k <= x+b ; k++){
 			var inc = game_col*y+k;
 			element = $('td[data-pos="'+inc+'"]');
+			element.addClass('placed-block');
+			element.addClass(block_owner);
 			element.css('background',color);
 		}
 		y = y+1;
 	}
+	block_moving = false;
+	dice_rolled = false;
+	$('#roll-block-btn').removeAttr("disabled");
+	$('#drop-block-btn').attr('disabled','true');
+	(plr==1) ? $('.p2_ownr').addClass('active-blocks') : $('.p1_ownr').addClass('active-blocks');
+	(plr==1) ? $('.p1_ownr').removeClass('active-blocks') : $('.p2_ownr').removeClass('active-blocks');
 }
 
 
 function CheckEmpire(x,y,l,b,key_state,plr){
+	$('td').removeClass('active-blocks');
+	$('.placed-block').css('opacity','0.5');
 	color = (plr==1) ? 'red' : 'blue';
 	if(key_state==0){ //up
 		if(!(y>=0)){
@@ -122,28 +137,15 @@ function CheckEmpire(x,y,l,b,key_state,plr){
 			return;
 		}
 	}
+	$('#drop-block-btn').removeAttr("disabled");
+	block_moving = true;
 	$('td').removeClass('check-location-1');
 	$('td').removeClass('check-location-2');
-	$('td').removeClass('active-left');
-	$('td').removeClass('active-right');
-	$('td').removeClass('active-top');
-	$('td').removeClass('active-bottom');
 	for(var i=x+1 ; i <= x+l ; i++){
 		for(var k=x+1 ; k <= x+b ; k++){
 			var inc = game_col*y+k;
 			element = $('td[data-pos="'+inc+'"]');
-			if(k==x+1){
-				element.addClass('active-left');
-			}
-			if(k==x+b){
-				element.addClass('active-right');
-			}
-			if(i==x+1){
-				element.addClass('active-top');
-			}
-			if(i==x+l){
-				element.addClass('active-bottom');	
-			}
+			element.css('opacity','1');
 			element.addClass('check-location-'+plr);
 		}
 		y = y+1;
@@ -252,6 +254,9 @@ function appendInsideDiceDiv(dice,code){
 function roll_dise(){
 	$('.dice-main-body').append(dice_val_one());
 	clearInterval(rolling);
+	if(fx_On){
+		$("#diceRollSound")[0].play();
+	}
 	rolling = setInterval(function(){
 		diceVal1 = getDiceDots(1,null);
 		diceVal2 = getDiceDots(2,null);
@@ -272,13 +277,11 @@ function showValues(ln,bn){
 	    content: 'length is :'+ln+'&nbsp;&nbsp;breadth is :'+bn,
 	});
 }
-document.onkeydown = checkKey;
-function checkKey(e) {
-	var t_x;
-	var t_y;
-    e = e || window.event;
-    if (e.keyCode == '38' && GameStarted) {
-        // up arrow
+
+
+function actionOnEvents(key){
+	if( key  == '38' && GameStarted && dice_rolled ){
+		 // up arrow
         key_state = 0;
         if(c_turn == 1){
         	if(!new_turn_1){
@@ -307,10 +310,8 @@ function checkKey(e) {
         }
         t_p = c_turn;
         CheckEmpire(t_x,t_y,l,b,key_state,t_p)
-        // createEmpire(t_x,t_y,l,b,key_state,t_p)
-    }
-    else if (e.keyCode == '40' && GameStarted) {
-        // down arrow
+	}else if( key == '40' && GameStarted && dice_rolled ){
+		// down arrow
         key_state = 1;
 	    if(c_turn == 1){
 	    	if(!new_turn_1){
@@ -341,9 +342,8 @@ function checkKey(e) {
 	        	new_turn_2 = false;
 	        }
 	    }
-    }
-    else if (e.keyCode == '37' && GameStarted) {
-       	// left arrow
+	}else if( key == '37' && GameStarted && dice_rolled){
+		// left arrow
        	key_state = 2;
        	if(c_turn == 1){
 	    	if(!new_turn_1){
@@ -374,9 +374,8 @@ function checkKey(e) {
 	        	new_turn_2 = false;
 	        }
 	    }
-    }
-    else if (e.keyCode == '39' && GameStarted) {
-       // right arrow
+	}else if( key == '39' && GameStarted && dice_rolled){
+		// right arrow
        	key_state = 3;
        	if(c_turn == 1){
 	    	if(!new_turn_1){
@@ -406,10 +405,9 @@ function checkKey(e) {
 	        	createEmpire(t_x,t_y,l,b,t_p);
 	        	new_turn_2 = false;
 	        }
-	    }
-    }
-    else if (e.keyCode==82 && GameStarted) {
-    	$('.dice-main-body').css('display','block');
+	    }	
+	}else if( key == '82' && GameStarted ){
+		$('.dice-main-body').css('display','block');
     	roll_dise();
     	t1 = l = (Math.floor(Math.random() * 6) + 1);
 		t2 = b = (Math.floor(Math.random() * 6) + 1);
@@ -421,11 +419,141 @@ function checkKey(e) {
 			appendInsideDiceDiv(1,d1.code);
 			appendInsideDiceDiv(2,d2.code);
 			showValues(t1,t2);
-		},2000);	
-    }else if(e.keyCode==13 && GameStarted){
-    	t_p = c_turn;
+		},2000);
+		dice_rolled = true;
+		$('#roll-block-btn').attr('disabled','true');
+	}else if( key == '13' && block_moving && GameStarted && dice_rolled ){
+		t_p = c_turn;
     	c_turn = (c_turn==1) ? 2 : 1;
     	createEmpire(valid_x,valid_y,l,b,t_p);
-    	$("#putSound")[0].play();
+    	if(fx_On){
+    		$("#putSound")[0].play();
+    	}
+	}else if( key == '80' && GameStarted ){
+		if(introSoundPlaying){
+			introSoundPlaying = false;
+			clearInterval(introSoundEvent);
+    		$("#introSound")[0].pause();
+		}else{
+			introSoundPlaying = true;
+			introSoundEvent = setInterval(function(){
+				$("#introSound")[0].play();
+			}, 10);
+		}
+	}else if( key == '70' && GameStarted ){
+		fx_On = fx_On ? false : true;
+	}
+}
+
+document.onkeydown = checkKey;
+function checkKey(e) {
+	var t_x;
+	var t_y;
+    e = e || window.event;
+    if (e.keyCode == '38' && GameStarted) {
+        // up arrow
+        actionOnEvents('38');
+    }
+    else if (e.keyCode == '40' && GameStarted) {
+        // down arrow
+        actionOnEvents('40');
+    }
+    else if (e.keyCode == '37' && GameStarted) {
+       	// left arrow
+       	actionOnEvents('37');
+    }
+    else if (e.keyCode == '39' && GameStarted) {
+       // right arrow
+       	actionOnEvents('39');
+    }
+    else if (e.keyCode==82 && GameStarted) {
+    	//roll the dice (r)
+    	actionOnEvents('82');	
+    }
+    else if(e.keyCode==13 && GameStarted){
+    	//enter and for drop the blocks
+    	actionOnEvents('13');
+    }
+    else if(e.keyCode=='80' && GameStarted){
+    	//key is p to play and stop intro music
+    	actionOnEvents('80');
+    }
+    else if(e.keyCode=='70' && GameStarted){
+    	//key is p to play and stop intro music
+    	actionOnEvents('70');
     }
 }
+
+
+function swipedetect(el, callback){
+    var touchsurface = el,
+    swipeCode,
+    startX,
+    startY,
+    distX,
+    distY,
+    threshold = 5,
+    restraint = 100,
+    allowedTime = 1000,
+    elapsedTime,
+    startTime,
+    handleswipe = callback || function(swipeCode){}
+    touchsurface.addEventListener('touchstart', function(e){
+        var touchobj = e.changedTouches[0]
+        swipeCode = null
+        dist = 0
+        startX = touchobj.pageX
+        startY = touchobj.pageY
+        startTime = new Date().getTime()
+        e.preventDefault()
+    }, false)
+  
+    touchsurface.addEventListener('touchmove', function(e){
+        e.preventDefault()
+    }, false)
+    touchsurface.addEventListener('touchend', function(e){
+        var touchobj = e.changedTouches[0]
+        distX = touchobj.pageX - startX
+        distY = touchobj.pageY - startY
+        elapsedTime = new Date().getTime() - startTime
+        if (elapsedTime <= allowedTime){
+            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ 
+                swipeCode = (distX < 0)? '37' : '39'
+            }
+            else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ 
+                swipeCode = (distY < 0)? '38' : '40'
+            }
+        }
+        handleswipe(swipeCode)
+        e.preventDefault()
+    }, false)
+}
+  
+var $zone = document.getElementById('game-board');
+swipedetect($zone, function(swipeCode){
+	if(swipeCode){
+		actionOnEvents(swipeCode);
+	}
+});
+$('#drop-block-btn').click(()=>{
+	actionOnEvents('13');	
+});
+$('#roll-block-btn').click(()=>{
+	actionOnEvents('82');
+});
+$('#change-music-setting').click(()=>{
+	if(introSoundPlaying){
+		$('#music-setting-img').attr('src','img/game_img/sound-off.svg');
+	}else{
+		$('#music-setting-img').attr('src','img/game_img/sound-on.svg');
+	}
+	actionOnEvents('80');
+});
+$('#change-fx-setting').click(()=>{
+	if(fx_On){
+		$('#fx-setting-img').attr('src','img/game_img/sound-off.svg');
+	}else{
+		$('#fx-setting-img').attr('src','img/game_img/sound-on.svg');
+	}
+	actionOnEvents('70');
+});
