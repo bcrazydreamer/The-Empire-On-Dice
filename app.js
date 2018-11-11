dc6a56bc27bf10ddaf9cca0a56bf7abeeb3d1ba2
Ccrazydreamer <bcrazydreamer@gmail.com>
2018-11-11 06:21:58 +0530

    testing

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ var session                 = require('express-session');
 var index                   = require('./routes/index');
 
 var app = express();
-app.set('views', path.join(__dirname, './views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/', index);
