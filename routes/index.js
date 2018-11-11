ce7aaaf82482f8ce60a3afcab828a637ec0e7922
Ccrazydreamer <bcrazydreamer@gmail.com>
2018-11-11 06:39:56 +0530

    renamed

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,8 @@ var bodyParser            = require('body-parser');
 var path                  = require('path');
 
 router.get('/',function(req, res, next) {
-    res.render('index', { title: 'Game'});
+    res.render('Index', { title: 'Game'});
 });
-//-------------------------------------------------------------------------------------------------------------------
-// router.post('/',function (req, res) {
-//   if(req.session.passport.user.role=="admin" || req.session.passport.user.role=="superadmin"){
-//         res.redirect('/Admin/Profile');
-//   }else {
-//         res.redirect('/community/communitypanel');
-//         //res.redirect('/Profile');
-//   }
-// });
+
 
 module.exports = router;
